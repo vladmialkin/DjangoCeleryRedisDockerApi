0bb6535c6ffbeebafc122fbf49eda4caf492e7de
programmer <myalkin.vlad@yandex.ru>
2024-04-01 20:05:58 +0300

    fix: Исправление имён url.

--- bigcorp/shop/urls.py
+++ bigcorp/shop/urls.py
@@ -4,7 +4,7 @@ from .views import products_view, products_detail_view, category_list_view
 app_name = 'shop'
 
 urlpatterns = [
-    path('', products_view, app_name='products'),
-    path('<slug:slug>/', products_detail_view, app_name='product_detail'),
-    path('search/<slug:slug>/', category_list_view, app_name='category_list'),
+    path('', products_view, name='products'),
+    path('<slug:slug>/', products_detail_view, name='product_detail'),
+    path('search/<slug:slug>/', category_list_view, name='category_list'),
 ]
