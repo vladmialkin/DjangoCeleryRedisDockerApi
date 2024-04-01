dc62dc3818a17790a92baeca3ad3f0d5b1e27f7e
programmer <myalkin.vlad@yandex.ru>
2024-04-01 20:05:38 +0300

    feat: Добавление url для объектов моделей Product и Category.

--- bigcorp/shop/models.py
+++ bigcorp/shop/models.py
@@ -36,8 +36,8 @@ class Category(models.Model):
             self.slug = slugify(rand_slug() + self.name)
         super().save(*args, **kwargs)
 
-    # def get_absolute_url(self):
-    #     return reverse("model_detail", kwargs={"pk": self.pk})
+    def get_absolute_url(self):
+        return reverse("shop:category_list", args=[self.slug])
 
 
 class Product(models.Model):
@@ -59,8 +59,8 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-    # def get_absolute_url(self):
-    #     return reverse("model_detail", kwargs={"pk": self.pk})
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.slug])
 
 
 class ProductManager(models.Manager):
