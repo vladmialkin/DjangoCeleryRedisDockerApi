493a04d58e3dce44d1456ec142ca7f674d759ada
programmer <myalkin.vlad@yandex.ru>
2024-04-01 21:49:52 +0300

    feat: Добавление url для корзины.

--- bigcorp/cart/urls.py
+++ bigcorp/cart/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from .views import cart_view
 
 
 app_name = 'cart'
 
 urlpatterns = [
-    path('', products_view, name='products'),
-    path('<slug:slug>/', products_detail_view, name='product_detail'),
-    path('search/<slug:slug>/', category_list_view, name='category_list'),
+    path('', cart_view, name='cart'),
+    # path('<slug:slug>/', products_detail_view, name='product_detail'),
+    # path('search/<slug:slug>/', category_list_view, name='category_list'),
 ]
